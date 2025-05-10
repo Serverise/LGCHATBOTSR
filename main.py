a9aa252f2cbf86ff2886333367efb1fad5a8d9ce
Serverise <tinalatkeblo@gmail.com>
2025-05-10 16:48:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,7 @@ import os
 import threading
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart, Command, Text
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.enums import ParseMode, ChatMemberStatus
 
 # Настройка логирования
